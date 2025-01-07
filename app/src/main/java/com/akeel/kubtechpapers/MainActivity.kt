f227759164e3e63ca98313c1600a3582d78d1660
Akeel shafi <akeelshafi20@gmail.com>
2025-01-07 15:37:58 +0530

    day 7

--- app/src/main/java/com/akeel/kubtechpapers/MainActivity.kt
+++ app/src/main/java/com/akeel/kubtechpapers/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.akeel.kubtechpapers
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -14,5 +17,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val pastPapers: CardView = findViewById(R.id.previous_year_question_papers)
+        val syllabus: CardView = findViewById(R.id.syllabus)
+
+        // Set an OnClickListener
+        pastPapers.setOnClickListener {
+            // Create an Intent to navigate to the second activity
+            val intent = Intent(this, EngineeringBranchesActivity::class.java)
+            startActivity(intent)
+        }
+        syllabus.setOnClickListener{
+            val intent = Intent(this,SyllabusActivity::class.java)
+            startActivity(intent)
+        }
+
+        // In an activity, for example when a button is clicked
+        val button = findViewById<CardView>(R.id.study_material)
+        button.setOnClickListener {
+            Toast.makeText(this, "This Section Will be Live Soon", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/akeel/kubtechpapers/SyllabusActivity.kt
+++ app/src/main/java/com/akeel/kubtechpapers/SyllabusActivity.kt
@@ -3,15 +3,14 @@ package com.akeel.kubtechpapers
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class EngineeringBranchesActivity : AppCompatActivity() {
+class SyllabusActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_engineering_branches)
+        setContentView(R.layout.activity_syllabus)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
