a4a41bff0327fcf512b731e6aa5e6f67120cb2a9
Akeel shafi <akeelshafi20@gmail.com>
2025-01-07 19:58:56 +0530

    day 7

--- app/src/main/java/com/akeel/kubtechpapers/EngineeringBranchesActivity.kt
+++ app/src/main/java/com/akeel/kubtechpapers/EngineeringBranchesActivity.kt
@@ -1,10 +1,8 @@
 package com.akeel.kubtechpapers
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -13,14 +11,12 @@ class EngineeringBranchesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_engineering_branches)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val button = findViewById<CardView>(R.id.computer_science)
-        button.setOnClickListener {
-            Toast.makeText(this, "This Section Will be Live Soon", Toast.LENGTH_SHORT).show()
         }
     }
 }
