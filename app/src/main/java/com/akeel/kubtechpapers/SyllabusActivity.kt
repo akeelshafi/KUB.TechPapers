20186f135990d4ce76ddc888b7863a22593f1a31
Akeel shafi <akeelshafi20@gmail.com>
2025-01-08 19:17:26 +0530

    day 7

--- app/src/main/java/com/akeel/kubtechpapers/SyllabusActivity.kt
+++ app/src/main/java/com/akeel/kubtechpapers/SyllabusActivity.kt
@@ -1,20 +1,42 @@
 package com.akeel.kubtechpapers
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
 
 class SyllabusActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_syllabus)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // CardViews
+        val cardUnit1 = findViewById<CardView>(R.id.syllabus_computer_engineering)
+        val cardUnit2 = findViewById<CardView>(R.id.syllabus_civil_engineering)
+        val cardUnit3 = findViewById<CardView>(R.id.syllabus_mechanical_engineering)
+        val cardUnit4 = findViewById<CardView>(R.id.syllabus_electrical_engineering)
+        val cardUnit5 = findViewById<CardView>(R.id.syllabus_electronics_engineering)
+
+        // URLs for each card
+        val urlUnit1 = "https://example.com/unit1"
+        val urlUnit2 = "https://example.com/unit2"
+        val urlUnit3 = "https://example.com/unit3"
+        val urlUnit4 = "https://example.com/unit4"
+        val urlUnit5 = "https://example.com/unit5"
+
+        // Setting up click listeners
+        cardUnit1.setOnClickListener { openWebView(urlUnit1) }
+        cardUnit2.setOnClickListener { openWebView(urlUnit2) }
+        cardUnit3.setOnClickListener { openWebView(urlUnit3) }
+        cardUnit4.setOnClickListener { openWebView(urlUnit4) }
+        cardUnit5.setOnClickListener { openWebView(urlUnit5) }
+    }
+
+    // Function to open WebViewActivity
+    private fun openWebView(url: String) {
+        val intent = Intent(this, WebViewActivity::class.java)
+        intent.putExtra("URL", url) // Passing the URL to WebViewActivity
+        startActivity(intent)
     }
 }
