7735ffc42700b4ac909e4a462481c28d12f8ba7c
Akeel shafi <akeelshafi20@gmail.com>
2025-01-16 18:51:32 +0530

    day 16

--- app/src/main/java/com/akeel/kubtechpapers/MainActivity.kt
+++ app/src/main/java/com/akeel/kubtechpapers/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.akeel.kubtechpapers
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,11 +17,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the custom toolbar
+        val toolbar: Toolbar = findViewById(R.id.custom_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "KU B.Tech Papers"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val pastPapers: CardView = findViewById(R.id.previous_year_question_papers)
         val syllabus: CardView = findViewById(R.id.syllabus)
         val result: CardView = findViewById(R.id.results)
@@ -28,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, EngineeringBranchesActivity::class.java)
             startActivity(intent)
         }
-        syllabus.setOnClickListener{
+        syllabus.setOnClickListener {
             val intent = Intent(this, SyllabusActivity::class.java)
             startActivity(intent)
         }
@@ -53,5 +65,64 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    // Inflate the menu (this will be used to add items to the toolbar)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bar, menu)
+        return true
+    }
+
+    // Handle item clicks in the menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about_us -> {
+                // Show "About Us" details in a dialog or a new activity
+                val intent = Intent(this, AboutUsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+
+            R.id.privacy_policy -> {
+                // Open the Privacy Policy in a browser
+                val intent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://www.example.com/privacy-policy"))
+                startActivity(intent)
+                true
+            }
+
+            R.id.check_for_updates -> {
+                // Check for app updates in the Play Store
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.example.app")
+                )
+                startActivity(intent)
+                true
+            }
+
+            R.id.share_app -> {
+                // Share the app with others
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                val shareText =
+                    "Check out this app: https://play.google.com/store/apps/details?id=com.example.app"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+                startActivity(Intent.createChooser(shareIntent, "Share App"))
+                true
+            }
+
+            R.id.rate_us -> {
+                // Open the Play Store page for the app to rate it
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.example.app")
+                )
+                startActivity(intent)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
---
+++ app/src/main/java/com/akeel/kubtechpapers/fragments/syllabus/MechanicalSyllabusFragment.kt
@@ -0,0 +1,60 @@
+package com.akeel.kubtechpapers.fragments.syllabus
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import com.akeel.kubtechpapers.R
+import com.akeel.kubtechpapers.WebViewActivity
+
+class MechanicalSyllabusFragment : Fragment() {
+
+    // URLs for each card
+    private val urlUnit1 = "https://drive.google.com/drive/folders/1C6oKSh4DLaM85NkmbZER_nuQkkJfPQ9C?usp=sharing"
+    private val urlUnit2 = "https://drive.google.com/drive/folders/1C6oKSh4DLaM85NkmbZER_nuQkkJfPQ9C?usp=sharing"
+    private val urlUnit3 = "https://drive.google.com/drive/folders/1C6oKSh4DLaM85NkmbZER_nuQkkJfPQ9C?usp=sharing"
+    private val urlUnit4 = "https://drive.google.com/drive/folders/1C6oKSh4DLaM85NkmbZER_nuQkkJfPQ9C?usp=sharing"
+    private val urlUnit5 = "https://drive.google.com/file/d/1GVnQn3TBgx3c4YSuTfk55oYV1MQmj97i/view?usp=sharing"
+    private val urlUnit6 = "https://drive.google.com/file/d/1GVnQn3TBgx3c4YSuTfk55oYV1MQmj97i/view?usp=sharing"
+    private val urlUnit7 = "https://drive.google.com/file/d/1GVnQn3TBgx3c4YSuTfk55oYV1MQmj97i/view?usp=sharing"
+    private val urlUnit8 = "https://drive.google.com/file/d/1GVnQn3TBgx3c4YSuTfk55oYV1MQmj97i/view?usp=sharing"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_mechanical_syllabus, container, false)
+
+        // CardViews
+        val cardUnit1 = view.findViewById<CardView>(R.id.card_sem1)
+        val cardUnit2 = view.findViewById<CardView>(R.id.card_sem2)
+        val cardUnit3 = view.findViewById<CardView>(R.id.card_sem3)
+        val cardUnit4 = view.findViewById<CardView>(R.id.card_sem4)
+        val cardUnit5 = view.findViewById<CardView>(R.id.card_sem5)
+        val cardUnit6 = view.findViewById<CardView>(R.id.card_sem6)
+        val cardUnit7 = view.findViewById<CardView>(R.id.card_sem7)
+        val cardUnit8 = view.findViewById<CardView>(R.id.card_sem8)
+
+        // Setting up click listeners for each card
+        cardUnit1.setOnClickListener { openWebView(urlUnit1) }
+        cardUnit2.setOnClickListener { openWebView(urlUnit2) }
+        cardUnit3.setOnClickListener { openWebView(urlUnit3) }
+        cardUnit4.setOnClickListener { openWebView(urlUnit4) }
+        cardUnit5.setOnClickListener { openWebView(urlUnit5) }
+        cardUnit6.setOnClickListener { openWebView(urlUnit6) }
+        cardUnit7.setOnClickListener { openWebView(urlUnit7) }
+        cardUnit8.setOnClickListener { openWebView(urlUnit8) }
+
+        return view
+    }
+
+    // Function to open WebViewActivity
+    private fun openWebView(url: String) {
+        val intent = Intent(activity, WebViewActivity::class.java)
+        intent.putExtra("URL", url) // Passing the URL to WebViewActivity
+        startActivity(intent)
+    }
+}
