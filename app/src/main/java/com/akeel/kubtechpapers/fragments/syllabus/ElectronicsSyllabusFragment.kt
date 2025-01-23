3241ae3405a52d6940b1d3d0989d1d0f8e9e1cc8
Akeel shafi <akeelshafi20@gmail.com>
2025-01-23 14:11:51 +0530

    day 23

--- app/src/main/java/com/akeel/kubtechpapers/fragments/syllabus/ElectronicsSyllabusFragment.kt
+++ app/src/main/java/com/akeel/kubtechpapers/fragments/syllabus/ElectronicsSyllabusFragment.kt
@@ -13,14 +13,14 @@ import com.akeel.kubtechpapers.WebViewActivity
 class ElectronicsSyllabusFragment : Fragment() {
 
     // URLs for each card
-    private val urlUnit1 = "https://drive.google.com/drive/folders/19aKoqJQaE6DR30HBprwcXz5e4m6bYK6y?usp=drive_link"
-    private val urlUnit2 = "https://drive.google.com/drive/folders/1y8gVeznU3nokeeWKrmMoafjO9eUhTq1z?usp=drive_link"
-    private val urlUnit3 = "https://drive.google.com/drive/folders/1z5ra8MvMAodKJumI0dNn85A5YfjDOt9u?usp=drive_link"
-    private val urlUnit4 = "https://drive.google.com/drive/folders/1jfu4JJeV_4LYtvpet27A2cT1dAzZx9e9?usp=drive_link"
-    private val urlUnit5 = "https://drive.google.com/drive/folders/18jHYsgXkR2rAoLXEqqijCWOw6bq7iDu1?usp=drive_link"
-    private val urlUnit6 = "https://drive.google.com/drive/folders/16gWEYdoMvMTHmBJUXLcWvIv7OMdb4Ds6?usp=drive_link"
-    private val urlUnit7 = "https://drive.google.com/drive/folders/1Rjbq1Ap7aBmFH4qTSw0p3fXGwc6tKqax?usp=drive_link"
-    private val urlUnit8 = "https://drive.google.com/drive/folders/1T7h2WIdek6EmF_IrTQUgt4CsLABRQKgK?usp=drive_link"
+    private val urlUnit1 = "https://drive.google.com/file/d/1BFnXmfoyaDkd1DBuZtLHrjhsxFGLmy1q/view?usp=drive_link"
+    private val urlUnit2 = "https://drive.google.com/file/d/1XQged8zTAwHFiV0Fwwoo5hCv_8yQ5Xrg/view?usp=drive_link"
+    private val urlUnit3 = "https://drive.google.com/file/d/1O7WxxCyetwxbbzRRiEKsO9iadDCQXBz_/view?usp=drive_link"
+    private val urlUnit4 = "https://drive.google.com/file/d/16RokOcTZGNR-HW2wH6h7BnKEwN3iXCIm/view?usp=drive_link"
+    private val urlUnit5 = "https://drive.google.com/file/d/1NVL1BV1shqvtDMKIwsKZ3Po-P8-0QS5U/view?usp=drive_link"
+    private val urlUnit6 = "https://drive.google.com/file/d/1Bosxm_bGyxEM1XfKM7u1RDepsm6fTMBG/view?usp=drive_link"
+    private val urlUnit7 = "https://drive.google.com/file/d/10wfbL610dPbnGsHmeasB5bXXA6g7c1Bk/view?usp=drive_link"
+    private val urlUnit8 = "https://drive.google.com/file/d/1r0_7NDgMA1N7sBCOOZJOVEoqAxwDVBPL/view?usp=drive_link"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
